973d58fb5246818b5f16276ae3bdf38c16b016ef
Achraf <hamrouni.pro@outlook.fr>
2021-02-03 18:35:39 +0100

    fix style notif

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,10 @@
     <UIHeader/>
     <main class="px-4 sm:px-6 pt-20 relative">
       <Nuxt/>
-      <transition name="slide-fade" mode="out-in">
-        <div v-if="$nuxt.isOffline" role="alert" class="mr-6 mb-6 z-10 text-sm fixed px-4 py-3 leading-normal text-red-100 bg-red-700 font-medium rounded right-0 bottom-0">
+      <transition  name="slide-fade" mode="out-in">
+        <div ref="notif" v-if="$nuxt.isOffline" role="alert" class="customWidth mx-4 sm:mx-6 mb-6 z-10 text-sm flex items-center justify-between fixed px-4 py-3 leading-normal text-red-100 bg-red-700 font-medium rounded right-0 bottom-0">
           Vous n'êtes plus connecté à internet.
+          <svg @click="closeNotif" class="ml-3 w-4 h-4" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="white" d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM11.4 10l2.83-2.83-1.41-1.41L10 8.59 7.17 5.76 5.76 7.17 8.59 10l-2.83 2.83 1.41 1.41L10 11.41l2.83 2.83 1.41-1.41L11.41 10z"/></svg>
         </div>
       </transition>
     </main>
@@ -21,6 +22,14 @@ export default {
   components: {
     UIHeader,
     UIFooter
+  },
+  methods: {
+    closeNotif() {
+      this.$refs.notif.style.opacity = '0'
+      setTimeout(() => {
+        this.$refs.notif.remove()
+      }, 200)
+    }
   }
 };
 </script>
@@ -36,11 +45,12 @@ export default {
 .slide-fade-enter-active {
   transition: all .3s ease;
 }
+
 .slide-fade-leave-active {
   transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
-.slide-fade-enter, .slide-fade-leave-to
-  /* .slide-fade-leave-active below version 2.1.8 */ {
+
+.slide-fade-enter, .slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
@@ -62,5 +72,15 @@ html {
   margin: 0;
   transition-duration: .2s;
   color: #1a1b1f;
+}
+
+.customWidth {
+  width: calc(100% - 30px);
+}
+
+@media all and (min-width: 480px) {
+  .customWidth {
+    width: unset;
+  }
 }
 </style>
